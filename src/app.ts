05b37c8a50fe5be1158d05a2066f4ceccf7afdda
sujanchakma1 <sujanckz926@gmail.com>
2026-01-07 22:21:39 +0600

    added better auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { postRouter } from "./modules/post/post.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
+
 const app = express();
 app.use(express.json())
+
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 
 app.use("/posts",postRouter)
